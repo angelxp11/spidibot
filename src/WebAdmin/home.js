9dfc635960ae5479b966993e89f999fbb63b4bcf
Jose Angel Bermúdez Choperena <jocheangel728@gmail.com>
2024-09-26 23:31:56 -0500

    ahora si prueba 1}

--- src/WebAdmin/home.js
+++ src/WebAdmin/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect ,useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
 import { useNavigate } from 'react-router-dom';
